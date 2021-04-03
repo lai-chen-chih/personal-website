d4402449d140f3865037f3c972e55f225098dacb
Casie <abao@math.ubc.ca>
2021-04-03 12:55:59 -0700

    initial commit

---
+++ precache-manifest.b6a19f26d372a73c9018e593c22f458c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58525475d962ae0b123c6934770bf86",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "cc79336e85078b83994a",
+    "url": "/personal-website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "380368e23f5a0a2f4bd5",
+    "url": "/personal-website/static/js/2.44a9d184.chunk.js"
+  },
+  {
+    "revision": "cc79336e85078b83994a",
+    "url": "/personal-website/static/js/main.e8f0ecac.chunk.js"
+  },
+  {
+    "revision": "2ed28986d76ad3bd96b5",
+    "url": "/personal-website/static/js/runtime-main.f2de4a92.js"
+  }
+]);
