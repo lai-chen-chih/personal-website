d9cfe414535a6e7f382c09500c194036baeff610
lai-chen-chih <56236410+lai-chen-chih@users.noreply.github.com>
2026-07-31 17:51:33 -0500

    update affiliation, publication, and presentations

---
+++ precache-manifest.4068ed781a32285d0b21cc2ecaa637de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd0bb10901c6bd3a8488f78ce8964d3",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "289aae180cdca651f78c",
+    "url": "/personal-website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "380368e23f5a0a2f4bd5",
+    "url": "/personal-website/static/js/2.44a9d184.chunk.js"
+  },
+  {
+    "revision": "289aae180cdca651f78c",
+    "url": "/personal-website/static/js/main.0710ff33.chunk.js"
+  },
+  {
+    "revision": "2ed28986d76ad3bd96b5",
+    "url": "/personal-website/static/js/runtime-main.f2de4a92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.48b2fc8fbf1a6c25e66842f366644035.js"
+  "/personal-website/precache-manifest.4068ed781a32285d0b21cc2ecaa637de.js"
 );
 
 self.addEventListener('message', (event) => {
